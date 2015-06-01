8c4f1f571ddb1e49149c91f24afdac2f2f498174
Eddem <adam.hajjami@gmail.com>
2015-06-01 13:23:11 +0200

    background

--- PigeonGame/World.cs
+++ PigeonGame/World.cs
@@ -9,12 +9,18 @@ namespace PigeonGame
 		Pidgy _pidgy;
 		Texture2D _pidgyTexture;
 		Vector2 _pidgyPosition ;
-		Game1 _g;
-
+//		Game1 _g;
+		Background _background;
+		Texture2D _bgTexture;
 
 
 		public World (Game1 g)
 		{
+
+			_bgTexture = g.Content.Load<Texture2D> ("w");
+//			_bgTexture.Height = g.GraphicsDevice.Viewport.Height;
+			_background = new Background(g, _bgTexture);
+
 			_pidgyTexture = g.Content.Load<Texture2D> ("Untitled");
 			_pidgyPosition = new Vector2 (30, g.GraphicsDevice.Viewport.Height - _pidgyTexture.Height - 30);
 
@@ -23,11 +29,14 @@ namespace PigeonGame
 
 		public void Update ()
 		{
-			_pidgy.Update ();	
+			_pidgy.Update ();
+			_background.Update ();
 		}
 
 		public void Draw (SpriteBatch spriteBatch)
 		{
+			_background.Draw (spriteBatch);
+
 			_pidgy.Draw (spriteBatch);
 
 		}
