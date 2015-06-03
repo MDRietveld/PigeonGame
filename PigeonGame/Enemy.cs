01adbc664279c6b262f88215fbce3841eff0cd23
Onurrr <onur38o@hotmail.com>
2015-06-03 10:40:47 +0200

    Enemy
    Enemy Class Added

---
+++ PigeonGame/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace PigeonGame
+{
+	class Enemy
+	{
+		private Texture2D _enemyTexture;
+		private Vector2 _myPosition;
+		public Game1 _game;
+
+
+		}
+	public class Enemy
+	{
+		public Enemy ()
+		{
+		}
+	}
+}
+
