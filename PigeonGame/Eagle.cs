59b21df1791198af04cf2d85d6eba6bc97c3fd36
NoDayMercy <martijnrietveld@msn.com>
2015-06-17 13:48:16 +0200

    Eagle sprite werkend!

--- PigeonGame/Eagle.cs
+++ PigeonGame/Eagle.cs
@@ -12,32 +12,36 @@ namespace PigeonGame
 		int _frames;
 		//int _frames2;
 		int _rij;
+		int size_hor;
+		int size_ver;
+		int _kolom;
 
 		//Rectangle _eagle;
 
 		public Eagle (Game1 g, World w, Texture2D texture, Vector2 position, int speed, float scale) : base (g, w, texture, position, speed, scale)
 		{
 			_speed = speed;
-			int size = _texture.Width/8;
+
+
+
+			size_hor = _texture.Width/8;
+			size_ver = _texture.Height / 2;
 			_rij = 1;
-			_sourceRectangle = new Rectangle (size * 0, size* _rij, size, size);
+			_kolom = 0;
+
+			_sourceRectangle = new Rectangle (size_hor * _kolom, size_ver* _rij, size_hor, size_ver);
+
 		}
 
-		//public Rectangle EaglePosition()
-		//{
-		//	return new Rectangle ((int)_position.X, (int)_position.Y, _texture.Width/5/12, _texture.Height/5/4);
-		//}
-
+		//  wordt gebruikt voor collision detection
 		public override Rectangle Bounds {
 			get { 
 				return new Rectangle ((int)_position.X, (int)_position.Y, _texture.Width/5/8, _texture.Height/5/2);
-				//return new Rectangle(0,0,_texture.Width, _texture.Height); 
 			}
 		}
 
 		public override void Update (GameTime gameTime)
 		{
-			int size = _texture.Width/8;
 			/*
 			if(pidgy.PigeonPosition().Intersects(EaglePosition()))
 			{
@@ -57,23 +61,23 @@ namespace PigeonGame
 				_elapsed = 0;
 
 			}
-			_sourceRectangle = new Rectangle (size *_frames, size* _rij, size, size);
+			_sourceRectangle = new Rectangle (size_hor *_frames, size_ver* _rij, size_hor, size_ver);
 				
 			_position.X += _speed;
-			//if (_position.X  > 300) 
-			//{
-			//_speed *= -1;
-			// rij= 1;
-			//}else if (_position.x <0 )
-			//{
-			//_speed*= -1;
-			//rij =0;
-			//}
+			if (_position.X  > 300) 
+			{
+			_speed *= -1;
+			 _rij= 0;
+			}else if (_position.X <0 )
+			{
+			_speed*= -1;
+			_rij =1;
+			}
 
 			//Check om de direction te bepalen
-			if (_speed < 0) {
-				Console.WriteLine("speed is -1");
-			}
+//			if (_speed < 0) {
+//				Console.WriteLine("speed is -1");
+//			}
 		}
 	}
 }
