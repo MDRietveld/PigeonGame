0a7afcf50c64ef9d82bf906ade679c2d2c3e77c8
NoDayMercy <martijnrietveld@msn.com>
2015-06-20 15:24:14 +0200

    Flying = op regen

--- PigeonGame/Pidgy.cs
+++ PigeonGame/Pidgy.cs
@@ -18,7 +18,6 @@ namespace PigeonGame
 		int _frames2;
 		int _rij;
 
-		float _cooldowntime = 0;
 		float _flying = 0;
 		float _regen = 0;
 		bool _flyup = true;
@@ -124,20 +123,19 @@ namespace PigeonGame
 			}
 
 			_regen += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-			_cooldowntime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-			if (_cooldowntime >= 3000f) 
-			{
-				_flyup = true;
-			}
-
-//			if (_flying += (float) gameTime.ElapsedGameTime.TotalMilliseconds)
 			Console.WriteLine (_flying);
 
 			if (_regen >= 300 && _flying >= 0) 
 			{
-				_flying -= 50;
+				_flying -= 100;
 				_regen = 0;
+			}
 
+			if (_flying >= 2000f) {
+				_flyup = false;
+				_flying = 2000f;
+			} else {
+				_flyup = true;
 			}
 
 			if (_keyboard.IsKeyDown (Keys.Up) && _flyup) {
@@ -149,12 +147,7 @@ namespace PigeonGame
 				}
 				_position.Y -= _fly.Y;
 				_sourceRectangle = new Rectangle (size * _frames, size* _rij, size, size);
-				if (_flying >= 2000f) 
-				{
-					_flyup = false;
-					_cooldowntime = 0;
-					_flying = 0;
-				}
+
 
 				if (_flying <= 0) 
 				{
