7a724cf651b0157a96e327d4c6b04fce4481c795
NoDayMercy <martijnrietveld@msn.com>
2015-06-23 19:40:19 +0200

    worm

---
+++ PigeonGame/Worm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PigeonGame
+{
+	public class Worm : GameObjects
+	{
+		public Worm (Game1 g, World w, Texture2D tex, Vector2 pos) :base (g, w, tex, pos)
+		{
+		}
+
+		public Rectangle PigeonPosition()
+		{
+			return new Rectangle ((int)_position.X, (int)_position.Y, _texture.Width/5/12, _texture.Height/5/4);
+		}
+
+
+	}
+}
+
