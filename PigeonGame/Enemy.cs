9f4ad940b44f57ebea6c627687afe7f5ff3d7bff
NeiChan <wesleylgr33@gmail.com>
2015-06-24 04:39:21 +0200

    SOURCE RECTANGLE ERROR FIX

--- PigeonGame/Enemy.cs
+++ PigeonGame/Enemy.cs
@@ -113,11 +113,10 @@ namespace PigeonGame
 				break;
 			case "Kangeroo":
 				_textureFrames = 1;
-
 				break;
 			}
 
-			_sourceRectangle = new Rectangle (size_hor * _frames, size_ver * _rij, size_hor, size_ver);
+			//_sourceRectangle = new Rectangle (size_hor * _frames, size_ver * _rij, size_hor, size_ver);
 
 //			rectangle = new Rectangle (size_hor * _frames, size_ver * _rij, _texture.Width/_textureFrames/5, _texture.Height/2/5);
 //
